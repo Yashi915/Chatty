285c1eebc6eb8d07247164f7d10c287708fb9c9a
Jatin Gupta <76555005+dev-jatin@users.noreply.github.com>
2023-12-09 14:31:25 +0530

    fix

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,37 +1,55 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Platform, SafeAreaView, Image } from "react-native";
 
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
-import { GiftedChat, Message } from "react-native-gifted-chat";
+import { Avatar, GiftedChat, IMessage, Message } from "react-native-gifted-chat";
+
+interface MessageProps extends IMessage {
+  messageType: string;
+}
 
 export default function TabOneScreen() {
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState<MessageProps[]>([]);
 
   useEffect(() => {
     setMessages([
       {
         _id: 1,
-        text: "Hello developer",
+        text: "hello",
         createdAt: new Date(),
         user: {
           _id: 2,
           name: "React Native",
-          avatar: "https://placeimg.com/140/140/any",
+          avatar: "https://dcblog.b-cdn.net/wp-content/uploads/2021/02/Full-form-of-URL-1.jpg",
         },
+        messageType: "hello",
       },
     ]);
   }, []);
 
-  const onSend = useCallback((messages = []) => {
+  const onSend = useCallback((messages: MessageProps[]) => {
     setMessages((previousMessages) => GiftedChat.append(previousMessages, messages));
   }, []);
 
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          style={{ height: 40, width: 40, borderRadius: 20 }}
+          source={{
+            uri: "https://dcblog.b-cdn.net/wp-content/uploads/2021/02/Full-form-of-URL-1.jpg",
+          }}
+        />
+        <View>
+          <Text style={styles.headingText}>APH chat</Text>
+          <Text style={styles.subHeadingText}>A few minutes</Text>
+        </View>
+      </View>
       <GiftedChat
         messages={messages}
-        onSend={(messages) => onSend(messages)}
+        onSend={(messages: MessageProps[]) => onSend(messages)}
         renderMessage={(props) => {
+          console.log("rpos", props);
           return <Message {...props} />;
         }}
         user={{
@@ -43,8 +61,54 @@ export default function TabOneScreen() {
 }
 
 const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#3047EC",
+    padding: 20,
+    gap: 30,
+    ...Platform.select({
+      android: {
+        paddingTop: 50,
+      },
+    }),
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  headingText: {
+    fontSize: 18,
+    lineHeight: 18,
+    fontFamily: "SpaceMono",
+    color: "white",
+    fontWeight: "700",
+  },
+  subHeadingText: {
+    fontSize: 14,
+    lineHeight: 18,
+    fontFamily: "SpaceMono",
+    color: "white",
+  },
   container: {
-    flex: 1,
+    ...Platform.select({
+      web: {
+        height: 723,
+        width: 450,
+        alignSelf: "center",
+        marginVertical: "auto",
+        borderRadius: 18,
+        overflow: "hidden",
+        shadowColor: "#000000",
+        shadowOffset: {
+          width: 0,
+          height: 3,
+        },
+        shadowOpacity: 0.17,
+        shadowRadius: 3.05,
+        elevation: 4,
+      },
+      android: {
+        flex: 1,
+      },
+    }),
+    backgroundColor: "white",
   },
   title: {
     fontSize: 20,
