c37dc77e3496a9d9863ba3660389eecb7f6dd63d
Jatin Gupta <76555005+dev-jatin@users.noreply.github.com>
2023-12-13 12:09:32 +0530

    make full faq

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,178 +1,268 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, TextInput, Button } from "react-native";
+import { StyleSheet, Text, View, Platform, Image, Pressable } from "react-native";
 
-const FAQComponent = () => {
-  const [userQuestion, setUserQuestion] = useState("");
-  const [answer, setAnswer] = useState("");
-  const [score, setScore] = useState("");
-  const [socket, setSocket] = useState(null);
+import { useCallback, useEffect, useState } from "react";
+
+import { Avatar, GiftedChat, IMessage, Message } from "react-native-gifted-chat";
+
+import { NlpManager } from "node-nlp-rn";
+
+import Faq from "../../assets/json/faq.json";
+
+import { AntDesign } from "@expo/vector-icons";
+
+interface MessageProps extends IMessage {
+  messageType: String;
+  relatedQuestionAnswer: [];
+}
+
+const manager = new NlpManager({ languages: ["en"], forceNER: true });
+
+export default function TabOneScreen() {
+  const [messages, setMessages] = useState<MessageProps[]>([]);
 
   useEffect(() => {
-    const newSocket = new WebSocket("ws://127.0.0.1:8000/ws");
+    const trainManager = async () => {
+      manager.addDocument("en", "HELLO", "HELLO");
 
-    newSocket.onopen = () => {
-      console.log("WebSocket connection opened");
+      manager.addAnswer("en", "Hello How are you", "HELLO");
+
+      for (let i = 0; i < Faq.length; i++) {
+        manager.addDocument("en", Faq[i].question, Faq[i].question);
+        manager.addAnswer("en", Faq[i].answer, Faq[i].question);
+      }
+
+      await manager.train();
     };
 
-    newSocket.onmessage = (message) => {
-      const data = JSON.parse(message.data);
-      setAnswer(data.answer);
-      setScore(data.score);
-    };
+    trainManager();
+  }, []);
 
-    newSocket.onclose = () => {
-      console.log("WebSocket connection closed");
-    };
+  const getResponse = async (question: String) => {
+    const res = await manager.process("en", question);
 
-    setSocket(newSocket);
+    let bestQustionLable = "";
+    let bestAccuracy = 0;
 
-    return () => {
-      newSocket.close();
-    };
-  }, []); // Empty dependency array to ensure the effect runs once on component mount
+    let relatedQuestionLable = [];
 
-  const handleAskQuestion = () => {
-    if (socket) {
-      socket.send(userQuestion);
+    for (let i = 0; i < res?.classifications?.length; i++) {
+      if (
+        res?.classifications[i]?.value > bestAccuracy &&
+        res?.classifications[i]?.label !== "None"
+      ) {
+        bestQustionLable = res?.classifications[i]?.label;
+        bestAccuracy = res?.classifications[i]?.value;
+      }
+      if (res?.classifications[i]?.value > 0.02 && res?.classifications[i]?.label !== "None") {
+        relatedQuestionLable.push(res?.classifications[i]?.label);
+      }
+    }
+
+    if (bestQustionLable) {
+      relatedQuestionLable = relatedQuestionLable.filter((item) => item !== bestQustionLable);
+    }
+
+    let bestAnswer = "";
+    let relatedQuestionAnswer = [];
+
+    for (let i = 0; i < Faq.length; i++) {
+      if (bestQustionLable && bestQustionLable === Faq[i]?.question) {
+        bestAnswer = Faq[i]?.answer;
+      }
+
+      for (let j = 0; j < relatedQuestionLable?.length; j++) {
+        if (relatedQuestionLable[j] === Faq[i]?.question) {
+          relatedQuestionAnswer.push(Faq[i]);
+        }
+      }
+    }
+
+    if (!bestAnswer && relatedQuestionAnswer?.length === 0) {
+      setMessages((previousMessages) =>
+        GiftedChat.append(previousMessages, {
+          _id: Math.random() * 1000000,
+          text: "I'm sorry, but I couldn't find a solution to your query. It's possible that your question is unique or not covered in the existing FAQs. Please consider rephrasing your question or providing more details, and I'll do my best to assist you. If the issue persists, you may also reach out to [customer support/contact information] for personalized assistance. Thank you for your understanding.",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "chat",
+            avatar: "https://dcblog.b-cdn.net/wp-content/uploads/2021/02/Full-form-of-URL-1.jpg",
+          },
+        })
+      );
+    }
+
+    if (bestAnswer) {
+      setMessages((previousMessages) =>
+        GiftedChat.append(previousMessages, {
+          _id: Math.random() * 1000000,
+          text: bestAnswer,
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "chat",
+            avatar: "https://dcblog.b-cdn.net/wp-content/uploads/2021/02/Full-form-of-URL-1.jpg",
+          },
+        })
+      );
+    }
+
+    if (relatedQuestionAnswer?.length !== 0) {
+      setMessages((previousMessages) =>
+        GiftedChat.append(previousMessages, {
+          _id: Math.random() * 1000000,
+          text: "Related Question",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "chat",
+            avatar: "https://dcblog.b-cdn.net/wp-content/uploads/2021/02/Full-form-of-URL-1.jpg",
+          },
+          messageType: "Related Question",
+          relatedQuestionAnswer: relatedQuestionAnswer,
+        })
+      );
     }
   };
 
+  const onSend = useCallback((messages: MessageProps[]) => {
+    setMessages((previousMessages) => GiftedChat.append(previousMessages, messages));
+
+    getResponse(messages[0]?.text);
+  }, []);
+
+  const RelatedQustionMessage = ({ ...props }) => {
+    const [shown, setShown] = useState(false);
+
+    return (
+      <View
+        style={{
+          backgroundColor: "white",
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.25,
+          shadowRadius: 3.84,
+
+          elevation: 5,
+          margin: 8,
+          padding: 8,
+          borderRadius: 8,
+        }}
+      >
+        <Text style={{ fontWeight: "bold" }}>Related Question</Text>
+
+        {props?.currentMessage?.relatedQuestionAnswer.map((item) => (
+          <>
+            <Pressable
+              onPress={() => setShown(true)}
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <Text style={{ flex: 1 }}>{item?.question}</Text>
+              <AntDesign name="right" size={20} color="black" />
+            </Pressable>
+            {shown ? <Text style={{ color: "grey" }}>{item?.answer}</Text> : null}
+          </>
+        ))}
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Text>Ask a Question:</Text>
-      <TextInput
-        style={{ height: 40, borderColor: "gray", borderWidth: 1, marginBottom: 10 }}
-        onChangeText={(text) => setUserQuestion(text)}
-        value={userQuestion}
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          style={{ height: 40, width: 40, borderRadius: 20 }}
+          source={{
+            uri: "https://dcblog.b-cdn.net/wp-content/uploads/2021/02/Full-form-of-URL-1.jpg",
+          }}
+        />
+        <View>
+          <Text style={styles.headingText}>APH chat</Text>
+          <Text style={styles.subHeadingText}>A few minutes</Text>
+        </View>
+      </View>
+      <GiftedChat
+        messages={messages}
+        onSend={(messages: MessageProps[]) => onSend(messages)}
+        renderMessage={(props) => {
+          if (props?.currentMessage?.messageType === "Related Question") {
+            return <RelatedQustionMessage {...props} />;
+          }
+
+          return <Message {...props} />;
+        }}
+        user={{
+          _id: 1,
+        }}
       />
-      <Button title="Ask" onPress={handleAskQuestion} />
-      <Text>Answer: {answer}</Text>
-      <Text>Score: {score}</Text>
     </View>
   );
-};
+}
 
-export default FAQComponent;
-
-// import { StyleSheet, Text, View, Platform, SafeAreaView, Image } from "react-native";
-//
-// import { useCallback, useEffect, useState } from "react";
-//
-// import { Avatar, GiftedChat, IMessage, Message } from "react-native-gifted-chat";
-//
-// interface MessageProps extends IMessage {
-//   messageType: string;
-// }
-//
-// export default function TabOneScreen() {
-//   const [messages, setMessages] = useState<MessageProps[]>([]);
-//
-//   useEffect(() => {
-//     setMessages([
-//       {
-//         _id: 1,
-//         text: "hello",
-//         createdAt: new Date(),
-//         user: {
-//           _id: 2,
-//           name: "React Native",
-//           avatar: "https://dcblog.b-cdn.net/wp-content/uploads/2021/02/Full-form-of-URL-1.jpg",
-//         },
-//         messageType: "hello",
-//       },
-//     ]);
-//   }, []);
-//
-//   const onSend = useCallback((messages: MessageProps[]) => {
-//     setMessages((previousMessages) => GiftedChat.append(previousMessages, messages));
-//   }, []);
-//
-//   return (
-//     <View style={styles.container}>
-//       <View style={styles.header}>
-//         <Image
-//           style={{ height: 40, width: 40, borderRadius: 20 }}
-//           source={{
-//             uri: "https://dcblog.b-cdn.net/wp-content/uploads/2021/02/Full-form-of-URL-1.jpg",
-//           }}
-//         />
-//         <View>
-//           <Text style={styles.headingText}>APH chat</Text>
-//           <Text style={styles.subHeadingText}>A few minutes</Text>
-//         </View>
-//       </View>
-//       <GiftedChat
-//         messages={messages}
-//         onSend={(messages: MessageProps[]) => onSend(messages)}
-//         renderMessage={(props) => {
-//           console.log("rpos", props);
-//           return <Message {...props} />;
-//         }}
-//         user={{
-//           _id: 1,
-//         }}
-//       />
-//     </View>
-//   );
-// }
-//
-// const styles = StyleSheet.create({
-//   header: {
-//     backgroundColor: "#3047EC",
-//     padding: 20,
-//     gap: 30,
-//     ...Platform.select({
-//       android: {
-//         paddingTop: 50,
-//       },
-//     }),
-//     flexDirection: "row",
-//     alignItems: "center",
-//   },
-//   headingText: {
-//     fontSize: 18,
-//     lineHeight: 18,
-//     fontFamily: "SpaceMono",
-//     color: "white",
-//     fontWeight: "700",
-//   },
-//   subHeadingText: {
-//     fontSize: 14,
-//     lineHeight: 18,
-//     fontFamily: "SpaceMono",
-//     color: "white",
-//   },
-//   container: {
-//     ...Platform.select({
-//       web: {
-//         height: 723,
-//         width: 450,
-//         alignSelf: "center",
-//         marginVertical: "auto",
-//         borderRadius: 18,
-//         overflow: "hidden",
-//         shadowColor: "#000000",
-//         shadowOffset: {
-//           width: 0,
-//           height: 3,
-//         },
-//         shadowOpacity: 0.17,
-//         shadowRadius: 3.05,
-//         elevation: 4,
-//       },
-//       android: {
-//         flex: 1,
-//       },
-//     }),
-//     backgroundColor: "white",
-//   },
-//   title: {
-//     fontSize: 20,
-//     fontWeight: "bold",
-//   },
-//   separator: {
-//     marginVertical: 30,
-//     height: 1,
-//     width: "80%",
-//   },
-// });
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#3047EC",
+    padding: 20,
+    gap: 30,
+    ...Platform.select({
+      android: {
+        paddingTop: 50,
+      },
+    }),
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  headingText: {
+    fontSize: 18,
+    lineHeight: 18,
+    fontFamily: "SpaceMono",
+    color: "white",
+    fontWeight: "700",
+  },
+  subHeadingText: {
+    fontSize: 14,
+    lineHeight: 18,
+    fontFamily: "SpaceMono",
+    color: "white",
+  },
+  container: {
+    ...Platform.select({
+      web: {
+        height: 723,
+        width: 450,
+        alignSelf: "center",
+        marginVertical: "auto",
+        borderRadius: 18,
+        overflow: "hidden",
+        shadowColor: "#000000",
+        shadowOffset: {
+          width: 0,
+          height: 3,
+        },
+        shadowOpacity: 0.17,
+        shadowRadius: 3.05,
+        elevation: 4,
+      },
+      android: {
+        flex: 1,
+      },
+    }),
+    backgroundColor: "white",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});
